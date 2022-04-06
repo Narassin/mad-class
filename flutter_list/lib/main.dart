dddb1f0f689ecb65ba26c572fafabc85ab8342ba
Narassin <37112149+Narassin@users.noreply.github.com>
2022-04-06 23:29:15 +0800

    Rewrite the scratch template of the app

--- flutter_list/lib/main.dart
+++ flutter_list/lib/main.dart
@@ -1,1 +1,27 @@
+// Coded by Captain N (Ahmad Nuruddin)
+// List app for MAD Class
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NApp());
+}
+
+class NApp extends StatelessWidget {
+  const NApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NList App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('NListing'),
+        ),
+        body: const Center(
+          child: Text('Add button Here [ここにおいてください]'),
+        ),
+      ),
+    );
+  }
+}
